2a49a1dcfccc153749bda16749a446e9ecad6a81
cloudy <cloud.kira@gmail.com>
2019-03-26 10:22:21 +0100

    tp2 done

--- TP2/exo3/PatternRecognition.java
+++ TP2/exo3/PatternRecognition.java
@@ -19,43 +19,68 @@ public class PatternRecognition
 				System.out.println(matcher.group(2));
 		}
 
-		getIPv4(args[0]);
+		int ipArray[] = getIPv4(args[0]);
+
+		if(ipArray == null)
+			System.out.println("this is NOT an IPv4 !");
+		else
+		{
+			System.out.println("this is an IPv4 !");
+			
+			for(int i = 0; i < 4; ++i)
+			{
+				System.out.print(ipArray[i]);
+				if(i < 3)
+					System.out.print(".");
+			}
+
+			System.out.println();
+		}
 	}
 
 	public static int[] getIPv4(String str)
 	{
-		int ipvArray[] = new int[4];
+		int ipArray[] = new int[4];
 
-		String ipPatternString = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
-							"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
-							"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
-							"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+		// This RegEx below checks the IP directly (without if)
+		// However I couldn't understand it all
+		// I keep it but I don't use it here
+
+		// String ipPatternString = "^([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		// 					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		// 					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])\\." +
+		// 					"([01]?\\d\\d?|2[0-4]\\d|25[0-5])$";
+
+		// Pattern ipPattern = Pattern.compile(ipPatternString);
+
+		// Matcher ipMatcher = ipPattern.matcher(str);
+		// boolean isIPv4 = ipMatcher.matches();
+
+		 // The RegEx below works if we only want to check a match
+		 // But doesn't create the 4 groups needed after
+		 // It only creates two : the first ([0-9]{1,3}) and the last ([0-9]{1,3})
+		
+		// String ipPatternString = "(?:([0-9]{1,3})\\.){3}([0-9]{1,3})";
+
+		String ipPatternString = "([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})\\.([0-9]{1,3})";
+		 // Compile pattern for further use
 		Pattern ipPattern = Pattern.compile(ipPatternString);
-
-		String ipPatternStringV2 = "(?:([0-9]{1,3})\\.){3}([0-9]{1,3})";
-		Pattern ipPatternV2 = Pattern.compile(ipPatternStringV2);
-
-		Matcher ipMatcherV2 = ipPatternV2.matcher(str);
-		boolean check = ipMatcherV2.matches();
-
-		if(!check || 
-				Integer.parseInt(matcher.group(1)) > 255 ||
-				Integer.parseInt(matcher.group(2)) > 255 ||
-				Integer.parseInt(matcher.group(3)) > 255 ||
-				Integer.parseInt(matcher.group(4)) > 255
-			)
-			System.out.println("this is not ipv4");
-
-
+		 // Check if the pattern and the string match
 		Matcher ipMatcher = ipPattern.matcher(str);
 		boolean isIPv4 = ipMatcher.matches();
 
-		if(isIPv4)
-			System.out.println("we have a good ipv4 :)");
-		else
-			System.out.println("meh");
+		if(!isIPv4 || 
+				Integer.parseInt(ipMatcher.group(1)) > 255 ||
+				Integer.parseInt(ipMatcher.group(2)) > 255 ||
+				Integer.parseInt(ipMatcher.group(3)) > 255 ||
+				Integer.parseInt(ipMatcher.group(4)) > 255
+			)
+			return null;
 
-		return ipvArray;
+		for(int i = 0; i < 4; ++i)
+			ipArray[i] = Integer.parseInt(ipMatcher.group(i+1));
+
+		return ipArray;
 	}
 }
 
