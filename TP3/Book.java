7dac53c1964cefa7ab481c6223f8eada4bae51ec
MARTI EMILIE <emarti@localhost.localdomain>
2019-04-01 17:06:15 +0200

    tp3 ex 2 & 3

--- TP3/Book.java
+++ TP3/Book.java
@@ -1,5 +1,48 @@
+import java.lang.Override;
+import java.util.Objects;
+
 public class Book
 {
 	private String title;
 	private String author;
+
+	public Book(String title, String author)
+	{
+		this.title = Objects.requireNonNull(title);
+		this.author = Objects.requireNonNull(author);
+	}
+
+	public static Book Book(String title)
+	{
+		return new Book(title, "<no author>");
+	}
+
+	public String getTitle()
+	{
+		return this.title;
+	}
+
+	public String getAuthor()
+	{
+		return this.author;
+	}
+
+	public boolean isSameBook(Book b)
+	{
+		return this.title.equals(b.title) && this.author.equals(b.author);
+	}
+
+	@Override
+	public boolean equals(Object obj)
+	{
+		Book myBook = (Book)obj;
+
+		return this.title.equals(myBook.title) && this.author.equals(myBook.author);
+	}
+
+	@Override
+	public String toString()
+	{
+		
+	}
 }
--- TP3/Main.java
+++ TP3/Main.java
@@ -1,9 +1,38 @@
+import java.util.ArrayList;
+
 public class Main
 {
 	public static void main(String[] args)
 	{
-		Book book = new Book();
-		System.out.println(book.title + ' ' + book.author);
+		// EXERCICE 1
+		// Book book = new Book("hello", "moto");
+		// Book book2 = Book.Book("Hello");
+		
+		// System.out.println(book.getTitle() + ' ' + book.getAuthor());
+		// System.out.println(book2.getTitle() + ' ' + book2.getAuthor());
+
+		// EXERCICE 2
+		// Book b1 = new Book("Da Java Code", "Duke Brown");
+		// Book b2 = b1;
+		// Book b3 = new Book("Da Java Code", "Duke Brown");
+
+		// System.out.println(b1 == b2);
+		// System.out.println(b1.isSameBook(b3));
+
+		// Book b1 = new Book("Da Java Code", "Duke Brown");
+		// Book b2 = b1;
+		// Book b3 = new Book("Da Java Code", "Duke Brown");
+
+		// ArrayList list = new ArrayList();
+		// list.add(b1);
+		// System.out.println(list.indexOf(b2));
+		// System.out.println(list.indexOf(b3));
+
+		Book aBook = new Book("Da Java Code", "Duke Brown");
+		Book anotherBook = new Book("<no title>", "<no author>");
+		ArrayList list = new ArrayList();
+		list.add(aBook);
+		System.out.println(list.indexOf(anotherBook));
 	}
 }
 
