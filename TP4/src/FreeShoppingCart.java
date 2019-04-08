ee27ac7ce7a227560d758455e6222f7483211741
MARTI EMILIE <emarti@localhost.localdomain>
2019-04-08 17:58:31 +0200

    one more method

--- TP4/src/FreeShoppingCart.java
+++ TP4/src/FreeShoppingCart.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.Optional;
 
 public class FreeShoppingCart
@@ -18,8 +19,57 @@ public class FreeShoppingCart
 		this.bookCounter++;
 	}
 	
-	public Optional<Book> longuestTitle()
+	public Optional<Book> longuestTitleWithIndex()
 	{
-
+		Book longuestTitleBook = this.shoppingCart.get(0);
+		
+		if(this.bookCounter == 0)
+			return Optional.empty();
+		else
+		{
+			for(int i = 0; i < this.bookCounter; ++i)
+				if(longuestTitleBook.getTitle().length() < this.shoppingCart.get(0).getTitle().length())
+					longuestTitleBook = this.shoppingCart.get(0);
+		}	
+		
+		return Optional.of(longuestTitleBook);
+	}
+	
+	public Optional<Book> longuestTitleWithIterator()
+	{
+		Book longuestTitleBook = this.shoppingCart.get(0);
+		
+		Iterator<Book> bookIterator = this.shoppingCart.iterator();
+		
+		if(this.bookCounter == 0)
+			return Optional.empty();
+		else
+		{
+			while(bookIterator.hasNext())
+			{
+				if(longuestTitleBook.getTitle().length() < this.shoppingCart.get(0).getTitle().length())
+					longuestTitleBook = this.shoppingCart.get(0);
+			}
+		}	
+		
+		return Optional.of(longuestTitleBook);
+	}
+	
+	public Optional<Book> longuestTitleWithForEach()
+	{
+		Book longuestTitleBook = this.shoppingCart.get(0);
+				
+		if(this.bookCounter == 0)
+			return Optional.empty();
+		else
+		{
+			for(Book eachBook : this.shoppingCart)
+			{
+				if(longuestTitleBook.getTitle().length() < this.shoppingCart.get(0).getTitle().length())
+					longuestTitleBook = this.shoppingCart.get(0);
+			}
+		}	
+		
+		return Optional.of(longuestTitleBook);
 	}
 }
